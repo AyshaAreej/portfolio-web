326fc53aaa4f1287cf3e88ca53fcd26c47b530dd
AyshaAreej01 <100909366+AyshaAreej01@users.noreply.github.com>
2025-08-22 12:50:49 +0500

    fix issue

---
+++ src/global.d.ts
@@ -0,0 +1,7 @@
+// Allow importing plain CSS files in TS/TSX
+declare module "*.css";
+
+// Explicitly declare Swiper's CSS entry points
+declare module "swiper/css";
+declare module "swiper/css/navigation";
+declare module "swiper/css/pagination";
